024956d3c115662f5d75a354be7ff30c4c7e68b7
logolica99 <51610096+logolica99@users.noreply.github.com>
2020-09-27 13:21:14 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import * as firebase from 'firebase'
+
 
 function App() {
+  useEffect(()=>{
+    getLocalUsers();
+  },[])
+  useEffect(()=>{
+    saveLocalusers();
+  })
+  const app =firebase.app()
+  console.log(app)
+  const [users,setUsers]=useState([])
+  const googleLogin = () =>{
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider)
+        .then(result=>{
+          const user = result.user;
+          //console.log(user)
+          setUsers([...users, user.displayName])
+   
+
+        })
+  }
+  
+  const saveLocalusers = () =>{
+  
+  localStorage.setItem('users', JSON.stringify(users));
+
+}
+const getLocalUsers =() =>{
+if(localStorage.getItem('users')===null){
+  localStorage.setItem('users',JSON.stringify([]))
+}else{
+  let userslocal =JSON.parse( localStorage.getItem('users'));
+  setUsers(userslocal)
+}
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello worlds</h1>
+      <button onClick={googleLogin}>Login with google</button>
+      {users.map(user=>(
+        <h3>Hello {user}</h3>
+      ))}
+
     </div>
   );
 }
 
 export default App;
+    //  {users.map(user => (
+    //     <h2>{user}</h2>
+
+    //   ))}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAr3kVxaqxgLhCHUQhh4x8HG2yRvHPR6hM",
+  authDomain: "hello-90860.firebaseapp.com",
+  databaseURL: "https://hello-90860.firebaseio.com",
+  projectId: "hello-90860",
+  storageBucket: "hello-90860.appspot.com",
+  messagingSenderId: "248530060899",
+  appId: "1:248530060899:web:fae9d803bda63963dfda56",
+  measurementId: "G-KE065RGQCZ"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
